2977461db793b353a0b52789c9d7a92437b55868
Sodynoizz_TH <tepza5556789@gmail.com>
2023-10-16 11:29:53 +0700

    update: tucmc auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { AuthProvider } from "tucmc-auth";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <AuthProvider TOKEN="tYjFVdtj_5jlF8GhpZ3QF49moVTNXRM6TzB4axMNADs=">
+        <Component {...pageProps} />
+      </AuthProvider>
+    </div>
+  );
 }
