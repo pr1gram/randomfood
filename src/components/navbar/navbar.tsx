e978b7a2bbab6e97a46978b55d3b9397979f5ff6
pr1gram <139366091+pr1gram@users.noreply.github.com>
2023-10-17 03:05:17 +0700

    fix Navbar Link

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,12 +1,14 @@
+import Link from "next/link"
+
 export default function Navbar() {
     return(
 <nav className="bg-white flex align-middle justify-center border-2 text-center z-20  ">
   <div className="flex justify-center">
     <div className=" border-r-2 w-20 text-center py-2 border-slate-900">
-        <a href="/">Home</a>
+        <Link href="/" >Home</Link>
     </div>
     <div className="  w-20 text-center py-2 border-slate-900">
-        <a href="/search">Search</a>
+        <Link href="/search">Search</Link>
     </div>
     <div className=" border-l-2 w-20 text-center py-2 border-slate-900">
         <a href="https://github.com/pr1gram/randomfood" target="_Blank">Github</a>
